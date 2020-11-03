8720bdca06f75fd9ac97afa6e014d8c4a939f0bf
zhz364 <zhz364@ucsd.edu>
2020-11-02 16:09:42 -0800

    Create player class

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
+import Player from"./player"
 const canvas = document.getElementById('game');
+const cxt = canvas.getContext("2d");
---
+++ src/player.js
@@ -0,0 +1,14 @@
+export default class Player {
+    constructor(x,y){
+        this.x = x;
+        this.y = y;
+        this.width = 34;
+        this.height = 52;
+        this.frameX = 0;
+        this.frameY = 0;
+        this.speed = 9;
+        this.moving = false;
+        
+    }
+}
+
